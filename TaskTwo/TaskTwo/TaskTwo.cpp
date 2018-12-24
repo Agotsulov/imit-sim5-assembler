142e65a11afca376a683a70a242054b8e55847a7
Agotsulov <agotsulov@gmail.com>
2018-12-25 00:14:21 +0600

    add sort task 2

--- TaskTwo/TaskTwo/TaskTwo.cpp
+++ TaskTwo/TaskTwo/TaskTwo.cpp
@@ -3,127 +3,119 @@
 
 #include "pch.h"
 #include <iostream>
-
+#include <stdio.h>
 
 /*
-15.   Найти  точку, из указанного массива точек, расстояние от  которой  до
-	 заданной плоскости Ax+By+Cx+D=0 будет наименьшим. Вычисление расстояние от
-	 точки до плоскости выделить в отдельную подпрограмму.
+15.  
+ Сравнение: сначала сравниваются поля count, а, в случае равенства, вершины  в
+ лексикографическом порядке.Отсортировать фигуры по убыванию.
 */
 
-struct Point
+struct Shape
 {
-	double x;
-	double y;
-	double z;
+	int count; 
+	int* vertexes;
 };
 
-double p(double A, double B, double C, double D, Point *point) {
-	double res = 0.0f;
-	double buff = 0.0f;
-	double x0 = point->x;
-	double y0 = point->y;
-	double z0 = point->z;
-	__asm {
-		finit;
-		fldz;
-		fld A;
-		fld A;
-		fmulp	st(1), st(0);
-		fld B;
-		fld B;
-		fmulp	st(1), st(0);
-		fld C;
-		fld C;
-		fmulp	st(1), st(0);
-		faddp	st(1), st(0);
-		faddp	st(1), st(0);
-		fsqrt;
-		fcomi  st(0), st(1);
-		jz ZERO;
-		fstp	qword ptr[buff];
-		mov ebx, point;
-		fld D;
-		fld C;
-		fld qword ptr[ebx + 16];
-		fmulp	st(1), st(0);
-		fld B;
-		fld qword ptr[ebx + 8];
-		fmulp	st(1), st(0);
-		fld A;
-		fld qword ptr[ebx];
-		fmulp	st(1), st(0);
-		faddp	st(1), st(0);
-		faddp	st(1), st(0);
-		faddp	st(1), st(0);
-		fld buff;
-		fdivp   st(1), st(0);
-		fstp	qword ptr[res];
-	ZERO:
-	}
+int compare(Shape *first, Shape *second) {
+	int res = 0;
+	res = second->count - first->count;
 	return res;
 }
 
-Point * find(double A, double B, double C, double D, Point **points, int n) {
-	Point *res = 0;
-	double oldP = 999999;
+void * sort(Shape **shapes, int n) {
+	int i = 0, j = 0;
+	int size_n = 0;
+	Shape *buff = 0;
+	Shape *a = 0; //DEBUG
+	Shape *b = 0; //DEBUG
 	__asm {
-		finit;
-		mov esi, 0;
+	SET:
+		mov eax, n;
+		dec eax;
+		dec eax;
+		mov ecx, 4;
+		mul ecx;
+		mov size_n, eax;
 		jmp START;
-	ITER:
-		add esi, 4;
+	ADD_I:
+		add i, 4;
+		mov j, 0;
+		jmp START;
+	ADD_J:
+		add j, 4;
+		jmp START;
+		
 	START:
-		mov ebx, points;
-		mov eax, [ebx + esi];
-
-	FILL:
-		sub esp, 36;
-		mov [esp], eax;
-		add esp, 4;
-		fld D;
-		fstp qword ptr[esp];
+		mov edx, shapes;
+		mov esi, j;
+		mov ebx, [edx + esi];
+		mov ecx, [edx + esi + 4];		
+	CHECK:
+		mov a, ebx; //DEBUG
+		mov b, ecx; //DEBUG
+		push ebx;
+		push ecx;
+		call compare;
 		add esp, 8;
-		fld C;
-		fstp qword ptr[esp];
-		add esp, 8;
-		fld B;
-		fstp qword ptr[esp];
-		add esp, 8;
-		fld A;
-		fstp qword ptr[esp];
-		sub esp, 60;
-		call p;
-		add esp, 60;
-	WHILE:
-		mov ecx, n;
+		cmp eax, 0;
+		jg SWAP;
+		jmp FOR_J;
+	SWAP:
+		mov edx, shapes;
+		mov esi, j;
+		mov ebx, [edx + esi];
+		mov ecx, [edx + esi + 4];
+		mov a, ebx; //DEBUG
+		mov b, ecx; //DEBUG
+		mov buff, ebx;
+		mov [edx + esi], ecx;
+		mov eax, buff;
+		mov [edx + esi + 4], eax;
+		jmp FOR_J;
+	FOR_J:
+		mov ecx, size_n;
+		mov esi, j;
 		cmp esi, ecx;
-		je ITER;
+		jl ADD_J;
+		jmp FOR_I;
+	FOR_I:
+		mov ecx, size_n;
+		mov esi, i;
+		cmp esi, ecx;
+		jl ADD_I;
+		jmp END;
 	END:
 	}
-	Point *res1 = res;
-	return res;
 }
 
 int main()
 {
-	Point **points = new Point* [3];
-	Point *point = new Point();
-	point->x = 1.1f;
-	point->y = 2.2f;
-	point->z = 3.3f;
-	Point *point2 = new Point();
-	point2->x = 5.1f;
-	point2->y = 7.2f;
-	point2->z = 8.3f;
-	Point *point3 = new Point();
-	point3->x = 1115.1f;
-	point3->y = 712.2f;
-	point3->z = 8611.3f;
-	points[0] = point;
-	points[1] = point2;
-	points[2] = point3;
-	find(1.2, 1.3, 1.4, 1.5, points, 3);
-	//p(1, 1, 1, 1, point);
+	Shape **s = new Shape*[5];
+
+	Shape *s1 = new Shape();
+	s1->count = 10;
+	Shape *s2 = new Shape();
+	s2->count = 2;
+	Shape *s3 = new Shape();
+	s3->count = 11;
+	Shape *s4 = new Shape();
+	s4->count = 6;
+	Shape *s5 = new Shape();
+	s5->count = 1;
+
+	s[0] = s1;
+	s[1] = s2;
+	s[2] = s3;
+	s[3] = s4;
+	s[4] = s5;
+
+	sort(s, 5);
+
+	for (int i = 0; i < 5; i++) {
+		printf("%i ", s[i]->count);
+	}
+	Shape *a = new Shape();
+	a->count = 1;
 }
 
